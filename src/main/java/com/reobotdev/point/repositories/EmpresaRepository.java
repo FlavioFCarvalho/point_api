b482a134d8a6376d5e3b5df811bcf8018f779af8
Flávio Carvalho <flaviocfr@gmail.com>
2019-10-21 16:04:51 -0300

    Criando EmpresaRepository

---
+++ src/main/java/com/reobotdev/point/repositories/EmpresaRepository.java
@@ -0,0 +1,13 @@
+package com.reobotdev.point.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.reobotdev.point.entities.Empresa;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long>{
+	
+	@Transactional(readOnly = true)
+	Empresa findByCnpj(String cnpj);
+
+}
